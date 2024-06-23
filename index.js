936753b0a4f922a917fbc171be12291cbc27b477
ismahil hosen <ismahilhosen4000@gmail.com>
2024-06-23 16:35:16 +0600

    first commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const app = express()
 const cors = require('cors');
+const morgan = require('morgan');
 const bodyParser = require('body-parser');
 require("./Config/db")
 
@@ -14,6 +15,7 @@ const PORT = process.env.PORT || 5000;
 //middeleware
 app.use(bodyParser.json())
 app.use(cors())
+app.use(morgan("dev"))
 
 app.get("/push" ,(req, res)=>{
     res.send("ping")
