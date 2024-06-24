c60a7ee5783b738f1be43bc4823b2e777bda47bb
ismahil hosen <ismahilhosen4000@gmail.com>
2024-06-24 08:10:28 +0600

    config

--- src/app.js
+++ src/app.js
@@ -1,16 +1,18 @@
 //intarnal impote
 const express = require('express');
 const app = express()
+const createError = require("http-errors");
 const cors = require('cors');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 require("./Config/db")
 
+
 //intarnal imports
 const authRoute = require('./Routes/authRoute');
 const ProductRoute = require("./Routes/ProductRoute")
 
-const PORT = process.env.PORT || 5000;
+
 
 //middeleware
 app.use(bodyParser.json())
@@ -21,9 +23,23 @@ app.get("/push" ,(req, res)=>{
     res.send("ping")
 })
 
+
+//client error
+
+app.use((req,res, next)=>{
+    next(createError(404, "route not found"))
+})
+
+//server error 
+app.use((err, res, req, next)=>{
+    res.status(err.status || 500).json({
+        success: false,
+        message: err.message
+    })
+})
+
 app.use("/api/v1/auth", authRoute)
 app.use("/api/v1/product", ProductRoute)
 
-app.listen(PORT, ()=>{
-    console.log(`app is ranning port ${PORT}`);
-})
+
+module.exports = app;
